c51aecf84b9d86469812f126b74cc2044683943d
mayukh2002 <62223416+mayukh2002@users.noreply.github.com>
2021-10-20 22:22:16 -0700

    New Branch for Breanna
    boom, new branch.

--- src/milestone3/Interval.java
+++ src/milestone3/Interval.java
@@ -2,12 +2,16 @@ package milestone3;
 
 import java.sql.Timestamp;
 
-public class Interval {
+public class Interval implements Comparable<Interval> {
     private Timestamp start, end;
 
-    public Interval(int[] a, int[] b){
-        start = new Timestamp(a[0], a[1], a[2], a[3], a[4], 0,0 );
-        end = new Timestamp( b[0], b[1], b[2], b[3], b[4], 0,0);
+    public Interval(int[] a, int[] b) {
+        /**
+         * TODO: We need to do a comparison right here to ensure that the start timestamp is before the end timestamp
+         */
+
+        start = new Timestamp(a[0], a[1], a[2], a[3], a[4], 0, 0);
+        end = new Timestamp(b[0], b[1], b[2], b[3], b[4], 0, 0);
     }
 
     public Timestamp getStart() {
@@ -28,8 +32,17 @@ public class Interval {
 
 
     @Override
-    public String toString(){
+    public String toString() {
         return start.toString() + " " + end.toString();
     }
 
+    @Override
+    public int compareTo(Interval o) {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        return true;
+    }
 }
--- src/milestone3JUnit/IntervalTests.java
+++ src/milestone3JUnit/IntervalTests.java
@@ -10,11 +10,28 @@ import static org.junit.Assert.*;
 public class IntervalTests {
 
     @Test
+    /**
+     * Test the compareTo() and equals() methods of the Interval class.
+     */
     public void test1(){
-        int[] a = {1920,21,21,21,21};
+        int[] a = {10,21,21,21,21};
         int[] b = {10,21,21,21,21};
-        Interval i = new Interval(a,b);
-        System.out.println(i);
+        Interval i1 = new Interval(a,b);
+
+        int[] c = {20,21,21,21,21};
+        int[] d = {100,21,21,21,21};
+        Interval i2 = new Interval(c,d);
+
+        Interval i3 = new Interval(c,d);
+
+        System.out.println(i1);
+        System.out.println(i2);
+
+        assertEquals(i1.compareTo(i2) == -1 , true);
+        assertEquals(i2.compareTo(i1) == 1 , true);
+        assertEquals(i2.compareTo(i3) == 0, true);
+        assertEquals(i3.equals(i2), true);
+        assertEquals(i1.equals(i3), false);
     }
 
 
