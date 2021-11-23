93483bf93855ebe7b1f9ca5123762d7c60dde9df
Saurabh <Saurabhkaran11@gmail.com>
2021-11-22 21:51:51 -0800

    Extra test cases by Abhinav

--- src/milestone5JUnit/CompareUserTests.java
+++ src/milestone5JUnit/CompareUserTests.java
@@ -31,4 +31,52 @@ public class CompareUserTests {
         CompareUserAvailability c6 = new CompareUserAvailability("Joy", 12, 20, 2050, 26);
         assertEquals(-1, c5.compareTo(c6));
     }
+
+    @Test //checks if they are the same
+    public void testOne() {
+        CompareUserAvailability a = new CompareUserAvailability("Bob", 9, 12, 2001, 12);
+        CompareUserAvailability b = new CompareUserAvailability("Bob", 9, 12, 2001, 12);
+
+        assertEquals(a.compareTo(b), 0);
+    }
+
+    @Test//confirms they are not the same
+    public void testTwo() {
+        CompareUserAvailability a = new CompareUserAvailability("Bob", 9, 12, 2001, 12);
+        CompareUserAvailability b = new CompareUserAvailability("Bob", 10, 12, 2001, 12);
+
+        assertNotEquals(a.compareTo(b), 0);
+    }
+
+    @Test//checks if months aren't equal
+    public void testThree() {
+        CompareUserAvailability a = new CompareUserAvailability("Bob", 9, 12, 2001, 12);
+        CompareUserAvailability b = new CompareUserAvailability("Bob", 10, 12, 2001, 12);
+
+        assertEquals(a.compareTo(b), -1);
+    }
+
+    @Test//checks if days arent equal
+    public void testFour() {
+        CompareUserAvailability a = new CompareUserAvailability("Bob", 9, 12, 2001, 12);
+        CompareUserAvailability b = new CompareUserAvailability("Bob", 9, 13, 2001, 12);
+
+        assertEquals(a.compareTo(b), -1);
+    }
+
+    @Test//checks if years arent equal
+    public void testFive() {
+        CompareUserAvailability a = new CompareUserAvailability("Bob", 9, 12, 2001, 12);
+        CompareUserAvailability b = new CompareUserAvailability("Bob", 9, 12, 2002, 12);
+
+        assertEquals(a.compareTo(b), -1);
+    }
+
+    @Test//checks if times arent equal
+    public void testSix() {
+        CompareUserAvailability a = new CompareUserAvailability("Bob", 9, 12, 2001, 12);
+        CompareUserAvailability b = new CompareUserAvailability("Bob", 9, 12, 2001, 13);
+
+        assertEquals(a.compareTo(b), -1);
+    }
 }
