ea43e0ffe47241b71e51c1f33fef19f664a7c328
Abhinav Selvaraj <32822740+abhinav-selvaraj@users.noreply.github.com>
2021-12-01 23:28:19 -0800

    DEMO

--- src/actualSwing/SecondFrame.java
+++ src/actualSwing/SecondFrame.java
@@ -12,6 +12,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import milestone3.Availability;
+import milestone3.Interval;
+
 
 public class SecondFrame extends JFrame implements ActionListener {
 	JLabel label;
@@ -27,8 +30,53 @@ public class SecondFrame extends JFrame implements ActionListener {
 	JButton endMinuteButton;
 	JButton endHourButton;
 	JButton addButton;
+	JButton submitButton;
+	
+	
+	
+	
+	//These are going to be hardcoded
+    int year = 21, month = 12, day = 17;
+    //These are going to be inputted
+    int startHour, startMin, endHour, endMin;
+    Availability user = new Availability();
+    Availability dummyUser = new Availability();
+
+    int[] a = {year, month, day, 21, 21};
+    int[] b = {year, month, day, 21, 21};
+    Interval i1 = new Interval(a, b);
+
+    int[] c = {year, month, day, 11, 21};
+    int[] d = {year, month, day, 21, 21};
+    Interval i2 = new Interval(c, d);
+
+    // Karan
+    int[] e = {year, month, day, 11, 46};
+    int[] f = {year, month, day, 12, 45};
+    Interval i3 = new Interval(e, f);
+
+    int[] g = {year, month, day, 11, 46};
+    int[] h = {year, month, day, 12, 45};
+    Interval i4 = new Interval(g, h);
+
+    // year, month, date, hour, minute
+
+    int[] i = {year, month, day, 21, 45};
+    int[] j = {year, month, day, 10, 45};
+    Interval i5 = new Interval(i, j);
+
+    int[] k = {year, month, day, 11, 46};
+    int[] l = {year, month, day, 12, 45};
+    Interval i6 = new Interval(k, l);
+
 		
 	SecondFrame() {
+		dummyUser.add(i1);
+        dummyUser.add(i2);
+        dummyUser.add(i3);
+        dummyUser.add(i4);
+        dummyUser.add(i5);
+        dummyUser.add(i6);
 		//Left Panel	
 		beigePanel = new JPanel();
 		beigePanel.setBackground(new Color(0xF5F5DC));
@@ -68,6 +116,19 @@ public class SecondFrame extends JFrame implements ActionListener {
 		endMinuteButton.setBackground(Color.LIGHT_GRAY);
 		endMinuteButton.setBounds(250,250,250,250);
 		
+		addButton = new JButton("add");
+		addButton.addActionListener(this);
+		addButton.setFocusable(false);
+		addButton.setBackground(Color.LIGHT_GRAY);
+		addButton.setBounds(100,100,50,50);
+		
+		submitButton = new JButton("submit");
+		submitButton.addActionListener(this);
+		submitButton.setFocusable(false);
+		submitButton.setBackground(Color.LIGHT_GRAY);
+		submitButton.setBounds(100,100,50,50);
+
+		
 		//Name Button
 		//nameButton = new JButton("Enter name");
 		//nameButton.addActionListener(this);
@@ -82,17 +143,18 @@ public class SecondFrame extends JFrame implements ActionListener {
 		//Text Field for ending hour
 		textFieldEndHour =  new JTextField();
 		textFieldEndHour.setPreferredSize(new Dimension(150,40));
-		textFieldEndHour.setBounds(150,150,150,150);
+		textFieldEndHour.setBounds(150,150,250,250);
 		
 		//Text Field for starting minute
 		textFieldStartMinute = new JTextField();	
 		textFieldStartMinute.setPreferredSize(new Dimension(150,40));
-		textFieldStartMinute.setBounds(250, 250, 250, 250);
+		textFieldStartMinute.setBounds(250, 150, 250, 250);
 		
 		//Text Field for ending minute
 		textFieldEndMinute = new JTextField();	
 		textFieldEndMinute.setPreferredSize(new Dimension(150,40));
-		textFieldEndMinute.setBounds(250, 250, 250, 250);
+		textFieldEndMinute.setBounds(350, 250, 250, 250);
+	
 			
 		this.getContentPane().setBackground(new Color(0xF5F5DC));
 		//this.add(label);
@@ -114,6 +176,8 @@ public class SecondFrame extends JFrame implements ActionListener {
 		
 		whitePanel.add(startMinuteButton);
 		whitePanel.add(endMinuteButton);
+		whitePanel.add(addButton);
+		whitePanel.add(submitButton);
 		
 		//whitePanel.add(nameButton);
 	}
@@ -121,24 +185,59 @@ public class SecondFrame extends JFrame implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String startMinute = "";
-			int startM = -1;
-			int startH = -1;
-			//name button
+			String endMinute = "";
+			String startH = "";
+			String endH = "";
+			
+			//start min button
 			if(e.getSource() == startMinuteButton) {
 				 startMinute = textFieldStartMinute.getText();
-				 startM = Integer.parseInt(startMinute);
-				 System.out.println(startM);
+				 startMin = Integer.parseInt(startMinute);
+				 System.out.println("Start min = " + startMin);
+				 
 				
 			}
 			
-			
-			String startHour = "";
-			//submit
-			if(e.getSource() == startHourButton) {
-				 startHour = textFieldStartHour.getText();
-				 startH = Integer.parseInt(startHour);
-				 System.out.println(startH);
+			//end min button
+			if(e.getSource() == endMinuteButton) {
+				 endMinute = textFieldEndMinute.getText();
+				 endMin = Integer.parseInt(endMinute);
+				 System.out.println("End min = " + endMin);
+				
 			}
+			//start hour button
+			if(e.getSource() == startHourButton) {
+				 startH = textFieldStartHour.getText();
+				 startHour = Integer.parseInt(startH);
+				 System.out.println("startHour =  " + startHour);
+				
+			}
+			//end hour button
+			if(e.getSource() == endHourButton) {
+				 endH = textFieldEndHour.getText();
+				 endHour = Integer.parseInt(endH);
+				 System.out.println("endHour = " + endHour);
+				
+			}
+			//add button
+			if(e.getSource() == addButton) {
+	            //this.dispose();
+	            //new SecondFrame();
+	            int[] a = {year, month , day, startHour, startMin, 0, 0};
+	            int[] b = {year, month , day, endHour, endMin, 0, 0};
+	            Interval i = new Interval(a,b);
+	            System.out.println("Interval Added " + i);
+	            user.add(i);
+	        }
+			
+			//submite button
+			if(e.getSource() == submitButton) {
+	            //this.dispose();
+	            //new SecondFrame();
+	            System.out.println(Availability.computeOverlap(user, dummyUser));
+	        }
+			
+			
 			
 			
 			
