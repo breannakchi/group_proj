cf985379f6aa8e874246860714fc782537b8b9ea
princessp3ach <89543173+princessp3ach@users.noreply.github.com>
2021-12-02 12:39:31 -0800

    updated: improved ui
    made the opening box a bit smaller to create centered look
    
    second page added "availability" and our project name label
    
    *also made it beige*
    
    currently working on a gradient theme

--- src/actualSwing/Frame.java
+++ src/actualSwing/Frame.java
@@ -51,10 +51,10 @@ public class Frame extends JFrame implements ActionListener {
 		loginButton.setFocusable(false);
 		loginButton.setBackground(Color.LIGHT_GRAY);
 		
-		this.setTitle("CU Later");
+		this.setTitle("CUL8R");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.setSize(500,500);
+		this.setSize(480,500);
 		this.setVisible(true);
 		this.add(titleLable);
 		this.add(catchline);
