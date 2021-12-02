c2f7b4e8cc2e46159d3d643324a9937aed7a71a9
Abhinav Selvaraj <32822740+abhinav-selvaraj@users.noreply.github.com>
2021-12-01 17:58:36 -0800

    Update Title Page

---
+++ src/actualSwing/Main.java
@@ -0,0 +1,13 @@
+package actualSwing;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Frame f = new Frame();
+		if(f.getFrame() == true) {
+			new Frame();
+		}
+	}
+}
