ef49cf11bbea7e5c45b95e87f48948b4cdd1a7b6
Abhinav Selvaraj <32822740+abhinav-selvaraj@users.noreply.github.com>
2021-12-01 22:47:45 -0800

    poo

--- src/actualSwing/SecondFrame.java
+++ src/actualSwing/SecondFrame.java
@@ -19,9 +19,14 @@ public class SecondFrame extends JFrame implements ActionListener {
 	JPanel whitePanel;
 	JTextField textFieldStartHour;
 	JTextField textFieldStartMinute;
+	JTextField textFieldEndHour;
+	JTextField textFieldEndMinute;
 	JButton startHourButton;
 	JButton nameButton;
 	JButton startMinuteButton;
+	JButton endMinuteButton;
+	JButton endHourButton;
+	JButton addButton;
 		
 	SecondFrame() {
 		//Left Panel	
@@ -35,12 +40,19 @@ public class SecondFrame extends JFrame implements ActionListener {
 		whitePanel.setBounds(300, 0, 300, 600);
 			
 			
-		//Start Time Button	
+		//Start Hour Button	
 		startHourButton = new JButton("Submit Start Hour");
 		startHourButton.addActionListener(this);
 		startHourButton.setFocusable(false);
 		startHourButton.setBackground(Color.LIGHT_GRAY);
 		startHourButton.setBounds(100,100,50,50);
+		
+		//Start Hour Button	
+		endHourButton = new JButton("Submit End Hour");
+		endHourButton.addActionListener(this);
+		endHourButton.setFocusable(false);
+		endHourButton.setBackground(Color.LIGHT_GRAY);
+		endHourButton.setBounds(100,100,50,50);
 		
 		//Start Minute Button
 		startMinuteButton = new JButton("Submit Start Min");
@@ -49,11 +61,18 @@ public class SecondFrame extends JFrame implements ActionListener {
 		startMinuteButton.setBackground(Color.LIGHT_GRAY);
 		startMinuteButton.setBounds(250,250,250,250);
 		
+		//End Minute Button
+		endMinuteButton = new JButton("Submit End Min");
+		endMinuteButton.addActionListener(this);
+		endMinuteButton.setFocusable(false);
+		endMinuteButton.setBackground(Color.LIGHT_GRAY);
+		endMinuteButton.setBounds(250,250,250,250);
+		
 		//Name Button
-		nameButton = new JButton("Enter name");
-		nameButton.addActionListener(this);
-		nameButton.setFocusable(false);
-		nameButton.setBackground(Color.LIGHT_GRAY);
+		//nameButton = new JButton("Enter name");
+		//nameButton.addActionListener(this);
+		//nameButton.setFocusable(false);
+		//nameButton.setBackground(Color.LIGHT_GRAY);
 		
 		//Text Field for starting hour
 		textFieldStartHour =  new JTextField();
@@ -61,9 +80,19 @@ public class SecondFrame extends JFrame implements ActionListener {
 		textFieldStartHour.setBounds(150,150,150,150);
 		
 		//Text Field for ending hour
+		textFieldEndHour =  new JTextField();
+		textFieldEndHour.setPreferredSize(new Dimension(150,40));
+		textFieldEndHour.setBounds(150,150,150,150);
+		
+		//Text Field for starting minute
 		textFieldStartMinute = new JTextField();	
 		textFieldStartMinute.setPreferredSize(new Dimension(150,40));
 		textFieldStartMinute.setBounds(250, 250, 250, 250);
+		
+		//Text Field for ending minute
+		textFieldEndMinute = new JTextField();	
+		textFieldEndMinute.setPreferredSize(new Dimension(150,40));
+		textFieldEndMinute.setBounds(250, 250, 250, 250);
 			
 		this.getContentPane().setBackground(new Color(0xF5F5DC));
 		//this.add(label);
@@ -73,10 +102,18 @@ public class SecondFrame extends JFrame implements ActionListener {
 		this.setTitle("Get User Input");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(600, 600);
+		
 		whitePanel.add(startHourButton);
+		whitePanel.add(endHourButton);
+		
 		whitePanel.add(textFieldStartHour);
+		whitePanel.add(textFieldEndHour);
 		whitePanel.add(textFieldStartMinute);
+		whitePanel.add(textFieldEndMinute);
+		
+		
 		whitePanel.add(startMinuteButton);
+		whitePanel.add(endMinuteButton);
 		
 		//whitePanel.add(nameButton);
 	}
